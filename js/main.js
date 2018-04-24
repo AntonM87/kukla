f766aad2a3ea888de6f2ff1044ab9b1b5b647b75
AntonM87 <31993176+AntonM87@users.noreply.github.com>
2018-04-24 11:46:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,17 +9,45 @@ $(document).ready(function () {
             '<i class="fa fa-angle-right" aria-hidden="true"></i>'
         ],
         autoplay: true,
-        autoplayTimeout: 1000
+        autoplayTimeout: 5000,
+        dots:true
     });
 });
 
-;(
-    function () {
-        var brandsContainer = document.querySelector('.brands-container');
+;(function () {
+    var brandsContainer = document.querySelector('.brands-container');
 
-        document.getElementById('allBrands').addEventListener('click',function () {
-            brandsContainer.classList.toggle('brands-container-h400');
-            brandsContainer.parentNode.classList.toggle('brands-h800');
-        });
-    }
-)();
+    document.getElementById('allBrands').addEventListener('click', function () {
+        brandsContainer.classList.toggle('brands-container-h400');
+        brandsContainer.parentNode.classList.toggle('brands-h800');
+    });
+
+
+    var pixelPerfectBG = document.getElementById('pixelPerfectBG');
+    var cursor = document.getElementById('cursor');
+
+    cursor.addEventListener('click', function (e) {
+        var target = e.target;
+        if (target.innerHTML == 'OFF') {
+            target.innerHTML = 'ON';
+            pixelPerfectBG.style.display = 'none';
+        }
+        else if (target.innerHTML == 'ON'){
+            target.innerHTML = 'OFF';
+            pixelPerfectBG.style.display = 'inline';
+        }
+        if(target.innerHTML == '1'){
+            pixelPerfectBG.style.opacity = 1;
+        }
+        if(target.innerHTML == '0.75'){
+            pixelPerfectBG.style.opacity = 0.75;
+        }
+        if(target.innerHTML == '0.5'){
+            pixelPerfectBG.style.opacity = 0.5;
+        }
+        if(target.innerHTML == '0.25'){
+            pixelPerfectBG.style.opacity = 0.25;
+        }
+    })
+
+})();
